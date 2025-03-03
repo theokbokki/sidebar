9d3eb1400ffb5fc27f19a80a8970e10b471b875d
théoo <90846584+theokbokki@users.noreply.github.com>
2025-03-03 07:47:31 +0100

    Add sidebar-link component

---
+++ resources/views/components/sidebar-link.blade.php
@@ -0,0 +1,6 @@
+    <a href="{{ $href }}" class="flex items-center gap-2 px-2 py-1.5 rounded-md hover:bg-neutral-200/50 {{ $attributes }}">
+        @isset($icon)
+            <span class="block w-5 h-5 text-neutral-600">{{ $icon }}</span>
+        @endisset
+        <span class="text-neutral-800">{{ $content }}</span>
+    </a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,5 +7,26 @@
         <title>Sidebar</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class=""></body>
+    <body class="flex w-screen h-screen">
+        <x-sidebar>
+            <div class="flex items-center justify-between px-5">
+                <a href="#"><x-icon-logo/></a>
+                <button class="p-1 cursor-pointer rounded-md hover:bg-neutral-200/50">
+                    <span class="sr-only">Navigation toggle</span>
+                    <span class="block w-5 h-5 text-neutral-500"><x-lucide-panel-left /></span>
+                </button>
+            </div>
+            <div class="flex-1 flex flex-col"></div>
+            <div class="flex flex-col gap-1 px-3">
+                <x-sidebar-link content="settings" href="#">
+                    <x-slot name="icon"><x-lucide-settings/></x-slot name="icon">
+                </x-sidebar-link>
+
+                <x-sidebar-link content="Contact Support" href="#">
+                    <x-slot name="icon"><x-lucide-life-buoy/></x-slot name="icon">
+                </x-sidebar-link>
+            </div>
+        </x-sidebar>
+        <main class="flex-1"></main>
+    </body>
 </html>
